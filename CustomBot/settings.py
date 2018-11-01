0f636394cc1739b067bfce89d0408e977b7ca9bc
Korb1t <oles.hobor@gmail.com>
2018-11-01 23:02:45 +0200

    finally

--- CustomBot/settings.py
+++ CustomBot/settings.py
@@ -112,5 +112,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = 'admin_panel/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
